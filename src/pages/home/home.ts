6247cb6c334973781666b5405bc5fe904fa5d4c3
Indravathi B <indravathi@hyd.pllcsoftwaresolutions.com>
2018-01-09 15:56:11 +0530

    fbbutton

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  FB_APP_ID: number = 495858277463909;
+  constructor(public navCtrl: NavController, 	public fb: Facebook,) {
+    this.fb.browserInit(this.FB_APP_ID, "v2.8");
+  }
+  connectwithfb(){
+    let permissions = new Array<string>();
+    let nav = this.navCtrl;
+	 let env = this;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
 
-  constructor(public navCtrl: NavController) {
 
+    this.fb.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array<string>();
+
+      //Getting name and gender properties
+      env.fb.api("/me?fields=name,gender", params)
+      .then(function(user) {
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //now we have the users info, let's save it in the NativeStorage
+        alert(user.name);
+        alert(user.gender);
+        alert(user.picture);
+        // env.nativeStorage.setItem('user',
+        // {
+        //   name: user.name,
+        //   gender: user.gender,
+        //   picture: user.picture
+        // })
+        // .then(function(){
+        //   nav.push(UserPage);
+        // }, function (error) {
+        //   console.log(error);
+        // })
+      })
+    }, function(error){
+     alert(error);
+    });
   }
 
+  connectwithgoogle(){
+    alert(9);
+  }
 }
